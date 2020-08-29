29d75e18d872235661a9235b4b73d5c1bedc199d
JAM <jmceldo1@mix.wvu.edu>
2020-08-29 16:02:52 -0400

    First pass at pieces, small issue rendering in simulator

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,28 @@
 var airconsole;
 var re = /images\/PNG-cards-1.3\/(.*)_of_/;
 var myTurn = false;
+var start = false;
 /**
  * Sets up the communication to the screen.
  */
 function init() {
+    // if (start) {
+    //     return;
+    // }
+    // start = true;
     console.log("Controller init function");
     airconsole = new AirConsole({ "orientation": "portrait" });
+    // setBoardBasedOnState();
+    // var testPiece = document.getElementById("piece");
+    // console.log(testPiece);
+
+    // var rs0 = document.getElementById("rs-0");
+
+    // var foo = getOffset(rs0);
+    // console.log(foo);
+
+    // testPiece.style.left = foo.left;
+    // testPiece.style.top = foo.top;
 
     /*
     * Checks if this device is part of the active game.
@@ -36,6 +52,9 @@ function init() {
                 doc.style.backgroundImage = new_value.playedCard;
             }
         });
+
+        //Will Call from here later JAM UNCOMMENT
+        setBoardBasedOnState();
     };
 
     airconsole.onMessage = function (device_id, data) {
@@ -70,6 +89,49 @@ function init() {
         div.innerHTML = newHtml;
     });
 }
+
+function setBoardBasedOnState() {
+    movePieceToSquare("rp-0", "rs-0");
+    movePieceToSquare("rp-1", "rs-1");
+    movePieceToSquare("rp-2", "rs-2");
+    movePieceToSquare("rp-3", "rs-3");
+
+    movePieceToSquare("yp-0", "ys-0");
+    movePieceToSquare("yp-1", "ys-1");
+    movePieceToSquare("yp-2", "ys-2");
+    movePieceToSquare("yp-3", "ys-3");
+
+    movePieceToSquare("gp-0", "gs-0");
+    movePieceToSquare("gp-1", "gs-1");
+    movePieceToSquare("gp-2", "gs-2");
+    movePieceToSquare("gp-3", "gs-3");
+
+    movePieceToSquare("bp-0", "bs-0");
+    movePieceToSquare("bp-1", "bs-1");
+    movePieceToSquare("bp-2", "bs-2");
+    movePieceToSquare("bp-3", "bs-3");
+
+}
+
+function movePieceToSquare(pieceId, elementId) {
+    var piece = document.getElementById(pieceId);
+    var element = document.getElementById(elementId);
+    var elementOffsets = getOffset(element);
+    piece.style.left = elementOffsets.left;
+    piece.style.top = elementOffsets.top;
+}
+ 
+function getOffset( el ) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
+
 
 
 
